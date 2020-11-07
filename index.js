ef393507103798257f39e15f617823cca42f9651
horothesun <horothesun@gmail.com>
2020-11-07 16:45:26 +0000

    All side effects moved into 'main'

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const getSignature = (apiSecret, timestamp, method, requestPath, body) => {
     return signature
 }
 
-const fetchData = async (requestPath, body, apiKey, apiSecret, timestamp, apiPassphrase) => {
+const fetchData = async (requestPath, body, apiKey, apiSecret, apiPassphrase, timestamp) => {
     const method = 'GET'
     const signature = getSignature(apiSecret, timestamp, method, requestPath, body)
     const response = await axios.get(`${baseUrl}${requestPath}`, {
@@ -43,11 +43,11 @@ const fetchData = async (requestPath, body, apiKey, apiSecret, timestamp, apiPas
 
 const zip = (arr1, arr2) => arr1.map((k, i) => [k, arr2[i]])
 
-const fetchTickersByCoin = async (fromCoins, toCurrency, apiKey, apiSecret, timestamp, apiPassphrase) => {
+const fetchTickersByCoin = async (fromCoins, toCurrency, apiKey, apiSecret, apiPassphrase, timestamp) => {
     const productIds = fromCoins.map(coin => `${coin}-${toCurrency}`)
     const tickerRequestPaths = productIds.map(productId => `/products/${productId}/ticker`)
     const tickers = await Promise.all(
-        tickerRequestPaths.map(path => fetchData(path, { }, apiKey, apiSecret, timestamp, apiPassphrase))
+        tickerRequestPaths.map(path => fetchData(path, { }, apiKey, apiSecret, apiPassphrase, timestamp))
     )
     const entries = new Map(zip(fromCoins, tickers))
     return Object.fromEntries(entries)
@@ -63,21 +63,20 @@ const getTotalValue = (fromCoins, accounts, tickersByCoin) => {
     return conversions.reduce((total, amount) => total + amount)
 }
 
-const main = async (apiKey, apiSecret, apiPassphrase, fromCoins, toCurrency) => {
+const main = async (fromCoins, toCurrency) => {
+    const apiKey = process.env.API_KEY
+    const apiSecret = process.env.API_SECRET
+    const apiPassphrase = process.env.API_PASSPHRASE
     const timestamp = Date.now() / 1000
+
     const [accounts, tickersByCoin] = await Promise.all([
-        fetchData('/accounts', { }, apiKey, apiSecret, timestamp, apiPassphrase),
-        fetchTickersByCoin(fromCoins, toCurrency, apiKey, apiSecret, timestamp, apiPassphrase)
+        fetchData('/accounts', { }, apiKey, apiSecret, apiPassphrase, timestamp),
+        fetchTickersByCoin(fromCoins, toCurrency, apiKey, apiSecret, apiPassphrase, timestamp)
     ])
     const totalValue = getTotalValue(fromCoins, accounts, tickersByCoin)
+
     console.log(`Total value (${toCurrency}): ${totalValue}`)
 }
 
-main(
-    process.env.API_KEY,
-    process.env.API_SECRET,
-    process.env.API_PASSPHRASE,
-    fromCoins,
-    toCurrency
-)
-.catch(console.log)
+main(fromCoins, toCurrency)
+    .catch(console.log)
